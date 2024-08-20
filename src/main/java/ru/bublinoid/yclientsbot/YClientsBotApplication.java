91b49173372e4c21704edf408518255406a21bd3
Hleb <gleb.qb@gmail.com>
2024-08-20 14:24:43 +0200

    init

---
+++ src/main/java/ru/bublinoid/yclientsbot/YClientsBotApplication.java
@@ -0,0 +1,13 @@
+package ru.bublinoid.yclientsbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YClientsBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YClientsBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/bublinoid/yclientsbot/YClientsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bublinoid.yclientsbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YClientsBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
